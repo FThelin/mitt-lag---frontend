a890eb71ad0ebc3766a265f768693d66e1bce39f
FThelin <fredrik@ixlayer.com>
2020-11-30 15:27:54 +0100

    setup

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Platform } from 'react-native';
 
 export default function App() {
+
+  const login = async () => {
+    const response = await fetch('http://localhost:5000/api/auth/login', {
+      method: "POST",
+      credentials: "include", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "fredrikthelin82@gmail.com",
+        password: "123456"
+      }),
+    });
+
+    const token = await response.json()  
+
+    if (Platform.OS === 'web') {
+      localStorage.setItem('token', token);
+  } else {
+      await SecureStore.setItemAsync('jwt', token);
+  }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hej</Text>
+      <Button onPress={() => login()}></Button>
+      <Button onPress={() => console.log(document.cookie)}></Button>
       <StatusBar style="auto" />
     </View>
   );
