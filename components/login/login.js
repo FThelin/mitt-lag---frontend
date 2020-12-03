ec1d968cc3c450672322c95e8fc1b868a5bbe572
FThelin <fredrik@ixlayer.com>
2020-12-03 13:15:58 +0100

    log in works..kinda

--- components/login/login.js
+++ components/login/login.js
@@ -2,19 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux"
 import { StyleSheet, Text, View, Button } from 'react-native';
-import authSlice from "../../features/auth/authSlice"
-
-const { setLoggedIn } = authSlice.actions;
+import {loginUser} from "../../features/auth/authSlice"
 
 export default function Login() {
   const dispatch = useDispatch()
-  const loggedIn = useSelector((state) => state.auth.loggedIn)
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
 
   return ( 
       <View style={styles.container}>
-      <Text>{loggedIn ? "Inloggad" : "Inte inloggad"}</Text>
-      <Button onPress={() => dispatch(setLoggedIn(true))} title="Logga in"/>
-      <Button onPress={() => console.log(loggedIn)} title="Test"/>
+        <Text>{isLoggedIn ? "Inloggad" : "Inte inloggad"}</Text>
+        <Button onPress={() => dispatch(loginUser({email: "fredrikthelin82@gmail.com", password: "123456"}))} title="Logga in"/>
+        <Button onPress={() => console.log(isLoggedIn)} title="Test"/>
         <StatusBar style="auto" />
       </View>
   );
--- features/auth/authSlice.js
+++ features/auth/authSlice.js
@@ -1,18 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 export const setLoggedIn = (state, action) => {
-    state.loggedIn = action.payload
+    state.isLoggedIn = action.payload
     return state
 }
+
+export const loginUser = createAsyncThunk(
+    'authSlice/loginUser',
+    async ({email, password}) => {
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+             "Content-Type": "application/json",
+             },
+            body: JSON.stringify({email, password}),
+            });
+            
+            const data = response.json()
+            
+            return data
+})
 
 const authSlice = createSlice({
     name: "authSlice",
     initialState: {
-        loggedIn: false
+        isLoggedIn: false,
+        isLoading: false,
+        showErrorMessage: false
     },
     reducers: {
         setLoggedIn
-    }
+    },
+    extraReducers: {
+        [loginUser.fulfilled]: (state, action) => {          
+          state.isLoggedIn = true
+          state.Loading = false
+          localStorage.setItem("token", action.payload)
+        },
+        [loginUser.pending]: (state) => { 
+            state.isLoggedIn = false         
+            state.Loading = true            
+        },
+        [loginUser.rejected]: (state) => { 
+            state.isLoggedIn = false         
+            state.Loading = false
+            state.showErrorMessage = true            
+        },
+      }
 })
 
 export default authSlice
