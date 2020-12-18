7971a777af3df4654d3778c470323b1d690b5627
FThelin <fredrik@ixlayer.com>
2020-12-18 14:19:28 +0100

    fixed bug

--- components/login/register.js
+++ components/login/register.js
@@ -73,7 +73,6 @@ export default function LoginDetails({ navigation }) {
               mode="outlined"
               value={inputValues.firstname}
               onChangeText={(e) => inputValue(e, "firstname")}
-              selectionColor="#purple"
             />
           </View>
           <View>
