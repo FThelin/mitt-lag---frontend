5fa40e54a86a38f56e1097265ec6d627f44214b9
Simon Beijer <beijer.simon@gmail.com>
2020-12-21 17:01:04 +0100

    last fix

--- features/auth/authSlice.js
+++ features/auth/authSlice.js
@@ -98,7 +98,7 @@ export const deleteJwt = async () => {
 const authSlice = createSlice({
   name: "authSlice",
   initialState: {
-    isLoggedIn: true,
+    isLoggedIn: false,
     isLoading: false,
     showLoginErrorMessage: false,
     showRegisterErrorMessage: false,
@@ -109,7 +109,7 @@ const authSlice = createSlice({
   },
   extraReducers: {
     [loginUser.fulfilled]: (state, action) => {
-      state.isLoggedIn = false;
+      state.isLoggedIn = true;
       state.isLoading = false;
       state.showLoginErrorMessage = false;
       saveJWT(action.payload);
